66de326cf00d1ca5a4f9c35ac9febeb2ca72b1f7
JeremyVe <jepschx@gmail.com>
2017-03-10 14:24:13 +0800

    added mongodb

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+var db = require('../db');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  var food = db.get().collection('movies').find().toArray(function(err, docs) {
+    console.log(docs);
+  })
   res.render('index', { title: 'Express' });
 });
 
